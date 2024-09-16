b9f83cc5bd0f9273e324009d6db7aeb09ec93bcc
binarybodhi <neohex262@gmail.com>
2024-09-16 14:25:59 +0400

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   port: process.env.PORT || 3000,
   mongoUri: process.env.MONGODB_URI || 'mongodb+srv://neohex:Gokzjiz30!@cluster0.ax82voh.mongodb.net/neurolov?retryWrites=true&w=majority&appName=Cluster0',
   jwtSecret: process.env.JWT_SECRET || '12345asdfghjkl09876123dsadfsdf',
-  telegramBotToken: process.env.TELEGRAM_BOT_TOKEN || '7220785507:AAHZxPT0YbdobIzWu-Ikmm46kbhIKSXIcss',
-  corsOrigin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://neurolovui.s3-website.eu-north-1.amazonaws.com',],
-  serverUrl: process.env.SERVER_URL || 'http://13.51.161.67',
-  webAppUrl: process.env.WEB_APP_URL || 'http://neurolovui.s3-website.eu-north-1.amazonaws.com',
+  telegramBotToken: process.env.TELEGRAM_BOT_TOKEN || '7498025356:AAF50Vd2lYx3YqopGg_3VMM0kxg_x_Ptro0',
+  corsOrigin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['https://neuro-orpin.vercel.app'],
+  serverUrl: process.env.SERVER_URL || 'http://16.16.234.176', 
+  webAppUrl: process.env.WEB_APP_URL || 'https://neuro-orpin.vercel.app',
 };
