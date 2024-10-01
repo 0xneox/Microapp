abfa6c037377350aa9451a12fa3f3edd18369f22
binarybodhi <neohex262@gmail.com>
2024-10-01 20:40:28 +0400

    Update rateLimiter.js

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,9 +1,9 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later.',
+  windowMs: 1 * 60 * 1000, // 1 min
+  max: 300, // Limit  IP to 300 requests/min
+  message: 'Too many taps! Please wait a moment before tapping again.',
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
